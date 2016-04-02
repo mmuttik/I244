1020bcb9f17574c428fed75c62644182aae9db91
mmuttik <marttimuttik@gmail.com>
2016-04-02 18:42:32 +0300

    syntax corrections

--- Week7/reverse.php
+++ Week7/reverse.php
@@ -1,19 +1,18 @@
 <?php
 
 $var = "Nii on ju eriti lihtne, aga samas igav.";
-echo $var;
-echo "</br>";
-echo "Tagurpidi: ";
-echo strrev($var);
-echo "</br>";
-echo "</br>";
+    echo $var;
+    echo "</br>";
+    echo "Tagurpidi: ";
+    echo strrev($var);
+    echo "</br></br>";
 
 
-$var2 = "Proovime siis teistmoodi.";
-echo $var2;
-$length = strlen($var2);
-echo "</br>";
-echo "Tagurpidi: ";
-for($i=$length-1;$i >=0;$i--){
+$var2 = "Proovime siis tsükliga.";
+  echo $var2;
+  $length = strlen($var2);
+  echo "</br>";
+  echo "Tagurpidi: ";
+  for($i=$length-1;$i >=0;$i--){
     echo $var2[$i];
 }
