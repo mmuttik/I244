79b66dd58433cfb5102ed073770d287835ba8e38
mmuttik <marttimuttik@gmail.com>
2016-04-04 17:55:06 +0300

    beautify the code

--- Week7/f1.php
+++ Week7/f1.php
@@ -1,15 +1,30 @@
 <?php
 /**
- * Created by IntelliJ IDEA.
- * User: X1
- * Date: 3.04.2016
- * Time: 18:59
- */
-$drivers= array(
-    // Megaaeglase internetiühenduse tõttu ei saanud lisada pilte ega muid atribuute!
-    array('name'=>'Raikkonen', 'team'=>'Ferrari', 'dream'=>'become a world champion once again', 'actual'=>'he is too old and slow!!!'),
-    array('name'=>'Alonso', 'team'=>'McLaren', 'dream'=>'become a world champion once again', 'actual'=>'he is driving with a slow car!!!'),
-    array('name'=>'Hakkinen', 'team'=>'McLaren', 'dream'=>'become a world champion once again', 'actual'=>'he is already retired!!!'),
+* Created by IntelliJ IDEA.
+* User: X1
+* Date: 3.04.2016
+* Time: 18:59
+*/
+$drivers = array(
+
+   array(
+       'name' => 'Raikkonen',
+       'team' => 'Ferrari',
+       'dream' => 'become a world champion once again',
+       'actual' => 'he is too old and slow!!!'
+   ),
+   array(
+       'name' => 'Alonso',
+       'team' => 'McLaren',
+       'dream' => 'become a world champion once again',
+       'actual' => 'he is driving with a slow car!!!'
+   ),
+   array(
+       'name' => 'Hakkinen',
+       'team' => 'McLaren',
+       'dream' => 'become a world champion once again',
+       'actual' => 'he is already retired!!!'
+   )
 );
 include('include.html');
 ?>
